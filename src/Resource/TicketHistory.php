98add2c76d9457d7605e7124d056fa05751f9e16
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-06-15 10:22:02 +0200

    up

--- src/Resource/TicketHistory.php
+++ src/Resource/TicketHistory.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @package Zammad API Client
- * @author  Jens Pfeifer <jens.pfeifer@znuny.com>
- */
-
 namespace Minvws\Zammad\Resource;
 
 use ZammadAPIClient\Resource\AbstractResource;
