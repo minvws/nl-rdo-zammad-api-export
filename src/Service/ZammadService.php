fe854afd418e3fe81902b9047eb3658396cb611c
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-06-20 11:24:51 +0200

    Added pagination and moved around some of the functionality

--- src/Service/ZammadService.php
+++ src/Service/ZammadService.php
@@ -22,10 +22,6 @@ class ZammadService
 
     protected array $groupCache = [];
 
-    /**
-     * @param string $url
-     * @param string $token
-     */
     public function __construct(string $url, string $token, HtmlGeneratorService $generator)
     {
         $this->generator = $generator;
@@ -55,82 +51,25 @@ class ZammadService
             'tickets' => [],
         ];
 
-        $tickets = $this->client->resource(ResourceType::TICKET)->all();
-        if (empty($tickets) && $this->verbose) {
-            $this->output->writeln("No tickets found.");
-        }
-        foreach ($tickets as $ticket) {
-            /** @var Ticket $ticket */
-            if ($group && $ticket->getValue('group_id') != $group->getID()) {
-                if ($this->verbose) {
-                    $this->output->writeln("* Skipping ticket(other group) " . $ticket->getID() . ' : '. $ticket->getValue('title'));
+        $page = 1;
+        while (true) {
+            if ($this->verbose) {
+                $this->output->writeln("Processing page $page");
+            }
+
+            $tickets = $this->client->resource(ResourceType::TICKET)->all($page, 100);
+            if (count($tickets) == 0) {
+                break;
+            }
+
+            foreach ($tickets as $ticket) {
+                if (! $this->shouldExport($ticket, $group, $percentage)) {
+                    continue;
                 }
-                continue;
+                $result = $this->exportTicket($ticket, $destinationPath, $result);
             }
-
-            // Fetch the MD5 of the ticket number (md5 should be evenly distributed). Fetch the first 2 digits to get
-            // a 0..255 value..  use the percentage (ie: 10% = 25, 50% =  128) to check if this ticket needs to be
-            // exported or not
-            $hash = md5($ticket->getValue('number'));
-            $hashValue = hexdec(substr($hash, 0, 2));
-            $minValue = intval(round($percentage * 2.55));
-            if ($hashValue > $minValue) {
-                continue;
-            }
-
-            $this->output->writeln("* Dumping ticket ".$ticket->getID().' : '.$ticket->getValue('title'));
-
-            $date = new \DateTime($ticket->getValue('created_at'));
-
-            $ticketGroup = $this->getGroupById($ticket->getValue('group_id'));
-            $ticketPath = $ticketGroup->getValue('name') . "/";
-            $ticketPath .= $date->format('Y-m') . "/";
-            $ticketPath .= $ticket->getValue('number');
-            $ticketPath = str_replace(":", "_", $ticketPath);
-
-            @mkdir($destinationPath . "/" . $ticketPath, 0777, true);
-            @mkdir($destinationPath . "/" . $ticketPath . "/articles", 0777, true);
-
-            // Dump ticket data
-            $data = json_encode($ticket->getValues(), JSON_PRETTY_PRINT);
-            file_put_contents($destinationPath . "/" . $ticketPath . "/ticket.json", $data);
-
-            $result['tickets'][] = [
-                'data' => $ticket->getValues(),
-                'path' => $ticketPath,
-            ];
-
-            // Dump tags
-            /** @var Tag $tag */
-            $tag = $this->client->resource(ResourceType::TAG)->get($ticket->getID(), 'Ticket');
-            $tags = $tag->getValue('tags');
-            file_put_contents($destinationPath . "/" . $ticketPath . "/tags.json", json_encode($tags, JSON_PRETTY_PRINT));
-
-            // Dump history
-            $history = $this->client->resource(TicketHistory::class)->get($ticket->getID());
-            $history = $history->getValues()['history'] ?? [];
-            file_put_contents($destinationPath . "/" . $ticketPath . "/history.json", json_encode($history, JSON_PRETTY_PRINT));
-
-            // Articles
-            $articles = $ticket->getTicketArticles();
-            foreach($articles as $article) {
-                $data = json_encode($article->getValues(), JSON_PRETTY_PRINT);
-
-                // Save article data
-                $articlePath = $destinationPath . "/" . $ticketPath . "/articles/" . $article->getID();
-                @mkdir($articlePath, 0777, true);
-                file_put_contents($articlePath . "/article.json", $data);
-
-                // Attachments
-                foreach($article->getValue('attachments') as $attachment) {
-                    $content = $article->getAttachmentContent($attachment['id']);
-                    file_put_contents($articlePath . "/". $attachment['filename'], $content);
-                }
-            }
-
-            $this->generator->generateTicket($destinationPath . "/" . $ticketPath, $ticket, $tags, $history);
+            $page++;
         }
-
         $this->generator->generateIndex($destinationPath, $result['tickets']);
     }
 
@@ -164,5 +103,85 @@ class ZammadService
 
         $this->groupCache[$groupId] = $group;
         return $group;
+    }
+
+    protected function exportTicket(Ticket $ticket, string $destinationPath, array $result): array
+    {
+        $this->output->writeln("* Dumping ticket ".$ticket->getID().' : '.$ticket->getValue('title'));
+
+        $date = new \DateTime($ticket->getValue('created_at'));
+
+        $ticketGroup = $this->getGroupById($ticket->getValue('group_id'));
+        $ticketPath = $ticketGroup->getValue('name') . "/";
+        $ticketPath .= $date->format('Y-m') . "/";
+        $ticketPath .= $ticket->getValue('number');
+        $ticketPath = str_replace(":", "_", $ticketPath);
+
+        @mkdir($destinationPath . "/" . $ticketPath, 0777, true);
+        @mkdir($destinationPath . "/" . $ticketPath . "/articles", 0777, true);
+
+        // Dump ticket data
+        $data = json_encode($ticket->getValues(), JSON_PRETTY_PRINT);
+        file_put_contents($destinationPath . "/" . $ticketPath . "/ticket.json", $data);
+
+        $result['tickets'][] = [
+            'data' => $ticket->getValues(),
+            'path' => $ticketPath,
+        ];
+
+        // Dump tags
+        /** @var Tag $tag */
+        $tag = $this->client->resource(ResourceType::TAG)->get($ticket->getID(), 'Ticket');
+        $tags = $tag->getValue('tags');
+        file_put_contents($destinationPath . "/" . $ticketPath . "/tags.json", json_encode($tags, JSON_PRETTY_PRINT));
+
+        // Dump history
+        $history = $this->client->resource(TicketHistory::class)->get($ticket->getID());
+        $history = $history->getValues()['history'] ?? [];
+        file_put_contents($destinationPath . "/" . $ticketPath . "/history.json", json_encode($history, JSON_PRETTY_PRINT));
+
+        // Articles
+        $articles = $ticket->getTicketArticles();
+        foreach($articles as $article) {
+            $data = json_encode($article->getValues(), JSON_PRETTY_PRINT);
+
+            // Save article data
+            $articlePath = $destinationPath . "/" . $ticketPath . "/articles/" . $article->getID();
+            @mkdir($articlePath, 0777, true);
+            file_put_contents($articlePath . "/article.json", $data);
+
+            // Attachments
+            foreach($article->getValue('attachments') as $attachment) {
+                $content = $article->getAttachmentContent($attachment['id']);
+                file_put_contents($articlePath . "/". $attachment['filename'], $content);
+            }
+        }
+
+        $this->generator->generateTicket($destinationPath . "/" . $ticketPath, $ticket, $tags, $history);
+
+        return $result;
+    }
+
+    protected function shouldExport(mixed $ticket, ?Group $group, int $percentage): bool
+    {
+        /** @var Ticket $ticket */
+        if ($group && $ticket->getValue('group_id') != $group->getID()) {
+            if ($this->verbose) {
+                $this->output->writeln("* Skipping ticket(other group) " . $ticket->getID() . ' : '. $ticket->getValue('title'));
+            }
+            return false;
+        }
+
+        // Fetch the MD5 of the ticket number (md5 should be evenly distributed). Fetch the first 2 digits to get
+        // a 0..255 value.  use the percentage (ie: 10% = 25, 50% =  128) to check if this ticket needs to be
+        // exported or not
+        $hash = md5($ticket->getValue('number'));
+        $hashValue = hexdec(substr($hash, 0, 2));
+        $minValue = intval(round($percentage * 2.55));
+        if ($hashValue > $minValue) {
+            return false;
+        }
+
+        return true;
     }
 }
