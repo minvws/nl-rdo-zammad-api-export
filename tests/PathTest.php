db83de32e9c3e9bec41489c8876157e2acc95957
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-07-14 08:04:21 +0200

    Added initial unittests

---
+++ tests/PathTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Minvws\Zammad\Path;
+use PHPUnit\Framework\TestCase;
+
+class PathTest extends TestCase {
+
+    public function testRootWithNonRoot()
+    {
+        $this->expectException(Exception::class);
+        new Path(new Path(null, "foo"), "bar", true);
+    }
+
+    public function testFromString()
+    {
+        $this->assertEquals("/foo/bar/baz", Path::fromString("/foo/bar/baz"));
+    }
+
+    public function testRoot()
+    {
+        $p = new Path(null, "foo", true);
+        $p = new Path($p, "bar");
+        $p = new Path($p, "/baz");
+        $this->assertEquals("/foo/bar/baz", $p);
+
+        $p = new Path(null, "foo");
+        $p = new Path($p, "bar");
+        $p = new Path($p, "/baz");
+        $this->assertEquals("foo/bar/baz", $p);
+    }
+
+    public function testAdd()
+    {
+        $p = (new Path(null, 'foo'))->add('bar')->add('baz');
+
+        $this->assertEquals('foo/bar/baz', $p->getPath());
+        $this->assertEquals('foo/bar/baz', $p);
+        $this->assertEquals('foo/bar/baz', (string)$p);
+    }
+
+    public function testSanitize()
+    {
+        $p = (new Path(null, 'fo/o!!#"$oéo'))->add("  /:b<<a\n\tr\\")->add('%*?@(/:baz)');
+
+        $this->assertEquals('foo!!$oéo/  bar/%@(baz)', $p->getPath());
+    }
+}
