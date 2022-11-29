f1adf057d0e73516f37974b2f6a54489ce740d8a
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-11-29 08:38:09 +0100

    Update FilenameSanitizer.php

--- src/FilenameSanitizer.php
+++ src/FilenameSanitizer.php
@@ -6,7 +6,7 @@ class FilenameSanitizer extends \IndieHD\FilenameSanitizer\FilenameSanitizer
 {
     public function stripAdditionalCharacters(): self
     {
-        $this->setFilename(str_replace(["'", '"', '#'], '', $this->getFilename()));
+        $this->setFilename(str_replace(["'", '#'], '', $this->getFilename()));
 
         return $this;
     }
